c76ff991f6c4a5d47515d74714fda4153a3a4246
Christopher Patton <chrispatton@gmail.com>
2014-05-20 11:02:05 -0700

    AssymetricGossip and moved q to dynamicsModel params.

--- simulation/sim.py
+++ simulation/sim.py
@@ -1,25 +1,31 @@
 # sim.py - Simulation of asynchronous consensus models.
-#
+# TODO
+#  - `q` specified in dynamics model, not by agent. 
 #
 import igraph
 import random
 
 #
 # Asynchronous opnion dynamics models. Nodes of `g` are expected to have 
-# an `Agent` attribute. 
+# an `Agent` attribute. `q` in [0 .. 1] is the weight of the alternate
+# opinion when updating. 
 #
 
-def SymmetricGossip(g):
-  # Choose an edge in `g` at random and have the nodes exchange opinions,
+def SymmetricGossip(g, q):
+  # Choose an edge in `g` uniformly and have the nodes exchange opinions,
   (u,v) = random.choice(list(g.es)).tuple
   opinion = g.vs[u]['agency'].opinion
-  g.vs[u]['agency'].UpdateOpinion(g.vs[v], g.vs[v]['agency'].opinion)
-  g.vs[v]['agency'].UpdateOpinion(g.vs[u], opinion)
+  g.vs[u]['agency'].UpdateOpinion(g.vs[v], g.vs[v]['agency'].opinion, q)
+  g.vs[v]['agency'].UpdateOpinion(g.vs[u], opinion, q)
 
-def AsymmetricGossip(g):
-  pass
+def AsymmetricGossip(g, q):
+  # choose a node from `g` uniformly and have it share its opinion with
+  # one of its neighbors, chosen uniformly. 
+  u = random.randint(0, len(g.vs) - 1) 
+  v = random.choice(g.vs[u].neighbors()).index
+  g.vs[u]['agency'].UpdateOpinion(g.vs[v], g.vs[v]['agency'].opinion, q)
 
-def Broadcast(g): 
+def Broadcast(g, q): 
   pass
 
 
@@ -27,19 +33,19 @@ def Broadcast(g):
 # Simulate opinion dynamics over `g` given a particular mode. Run 
 # until convergence, at most `rounds` times. If no agents are 
 # provided, standard agents with initial opinion between 0 and 10 
-# and q = 0.5 are generated. 
+# are generated. 
 #
 
 class Simulation: 
   
-  def __init__(self, g, agents=None, q=0.5):
+  def __init__(self, g, agents=None):
     self.g = g
     if agents:
       assert len(agents) == len(g.vs)
       selg.g.vs['agency'] = agents
     else:
       self.g.vs['agency'] = [ 
-                  Agent(random.uniform(0,10), q) for i in range(len(g.vs)) ]
+                  Agent(random.uniform(0,10)) for i in range(len(g.vs)) ]
 
   def SetOpinionsUnif(self, low, high):
     # Set opinions by selecting a real value uniformly high and low. 
@@ -63,11 +69,11 @@ class Simulation:
     # Return a list of (Vertex, Agent) pairs.
     pass
 
-  def Simulate(self, DynamicsModel=SymmetricGossip, rounds=1000):
+  def Simulate(self, dynamicsModel=SymmetricGossip, q=0.5, rounds=1000):
     # TODO check for convergence
     
     for i in range(rounds):
-      DynamicsModel(self.g)
+      dynamicsModel(self.g, q)
     
     return [ agent.opinion for agent in self.g.vs['agency'] ]
       
@@ -82,9 +88,8 @@ class Agent:
   # Standard agent type. Constructor accepts an initial opinion
   # and `q` in [0 .. 1], the weight of alternative opinions. 
 
-  def __init__(self, initialOpinion, q):
+  def __init__(self, initialOpinion):
     self.initialOpinion = self.opinion = initialOpinion
-    self.q = q
 
   def RestoreInitialOpinion(self):
     self.opinion = self.initialOpinion
@@ -92,18 +97,18 @@ class Agent:
   def SetOpinion(self, opinion):
     self.opinion = opinion
 
-  def UpdateOpinion(self, anAgent, altOpinion): 
-    self.opinion = ((1 - self.q) * self.opinion) + (self.q * altOpinion)
+  def UpdateOpinion(self, anAgent, altOpinion, q): 
+    self.opinion = ((1 - q) * self.opinion) + (q * altOpinion)
 
 
 class StubbornAgent (Agent): 
   
   # Stubborn agents simply don't change their minds. 
 
-  def __init__(self, initialOpinion, q):
-    Agent.__init__(initialOpinion, q)
+  def __init__(self, initialOpinion):
+    Agent.__init__(initialOpinion)
 
-  def UpdateOpinion(self, anAgent, altOpinion): 
+  def UpdateOpinion(self, anAgent, altOpinion, q): 
     pass
 
 
@@ -111,11 +116,11 @@ def ReluctantAgent (Agent):
 
   # Reluctant agents. 
   
-  def __init__(self, initialOpinion, q, updateRate):
-    Agent.__init__(initialOpinion, q)
+  def __init__(self, initialOpinion, updateRate):
+    Agent.__init__(initialOpinion)
     self.tao = updateRate 
      
-  def UpdateOpinion(self, anAgent, altOpinion): # TODO
+  def UpdateOpinion(self, anAgent, altOpinion, q): # TODO
     pass
 
 
@@ -127,7 +132,7 @@ if __name__ == '__main__':
 
 
   sim = Simulation(g)
-  print sim.Simulate(rounds=100)
+  print sim.Simulate(dynamicsModel=AsymmetricGossip, rounds=1000)
 
 
   style = {}
