e9a3b7c3de3e08857730649e39f1f2eacc5bffd4
Christopher Patton <chrispatton@gmail.com>
2014-05-28 16:35:47 -0700

    Using *less* FB data.

--- face.py
+++ face.py
@@ -9,26 +9,36 @@ def exp(fn, g, agents, trials):
   for trial in range(trials):
     print "did this", trial
     while not sim.TestConvergence(): 
-      print "got here"
       sim.Run(dynamicsModel=consensus.SymmetricGossip, q=0.5, rounds=100000)
     fd.write('%f,%d\n' % (sim.GetConsensus()[0], sim.TimeOfConvergence())) 
     sim.Reset()
   fd.close()
 
 
-trials = 1
+trials = 1000
 
-n = 4039
-g = igraph.Graph()
+vs = set()
 es = []
-fd = open("data/facebook_combined.txt", "r")
+fd = open("data/facebook.edges", "r")
 for line in map(lambda(l) : l.strip().split(' '), fd.readlines()):
   (u, v) = int(line[0]), int(line[1])
+  vs.add(u)
+  vs.add(v)
   es.append((u,v))
 fd.close()
+fella = {}
+for (i, u) in enumerate(vs): 
+  fella[u] = i
+guy = []
+for (u, v) in es: 
+  guy.append((fella[u], fella[v]))
+es = guy
+n = len(vs)
+
+g = igraph.Graph()
 g.add_vertices(n)
 g.add_edges(es)
-
+print n
 i = random.randint(0,n-1)
 
 agents = [ consensus.Agent(1) for i in range(n) ]
@@ -43,5 +53,5 @@ control_agents[i] = consensus.Agent(100)
 print "Facebook"
 exp("facebook.csv", g, agents, trials) 
 print "Facebook (control)"
-exp("erdosrenyi_control.csv", g, control_agents, trials)
+exp("facebook_control.csv", g, control_agents, trials)
 
